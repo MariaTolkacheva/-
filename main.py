b3f3515d0f077bd6bb902dcd674adee5c3527000
MariaTolkacheva <mari.tolkacheva.04@mail.ru>
2022-09-27 23:58:27 +0300

    new

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 print('задание по Технологии Программирования')
+
+
 class Contact:
     everybody = {}
     all_names1 = {}
@@ -9,7 +11,7 @@ class Contact:
     all_emails = {}
     no_emails = []
     no_phones = []
-    id = 1
+    id = 0
 
     def __init__(self, name, phone, email):
         Contact.everybody[Contact.id] = [name, phone, email]
@@ -17,39 +19,39 @@ class Contact:
         self.name = name
         n = name.split()
         if n[0] in Contact.all_names1.keys():
-            Contact.all_names1[n[0]].append([name, phone, email])
+            Contact.all_names1[n[0]].append(Contact.id)
         else:
-            Contact.all_names1[n[0]] = [[name, phone, email]]
+            Contact.all_names1[n[0]] = [Contact.id]
         if len(n) > 1:
             if n[1] in Contact.all_names2.keys():
-                Contact.all_names2[n[1]].append([name, phone, email])
+                Contact.all_names2[n[1]].append(Contact.id)
             else:
-                Contact.all_names2[n[1]] = [[name, phone, email]]
+                Contact.all_names2[n[1]] = [Contact.id]
 
             if n[0] + ' ' + n[1] in Contact.all_names12.keys():
-                Contact.all_names12[n[0] + ' ' + n[1]].append([name, phone, email])
+                Contact.all_names12[n[0] + ' ' + n[1]].append(Contact.id)
             else:
-                Contact.all_names12[n[0] + ' ' + n[1]] = [[name, phone, email]]
+                Contact.all_names12[n[0] + ' ' + n[1]] = [Contact.id]
 
         if len(n) > 2:
             if n[1] + ' ' + n[2] in Contact.all_names23.keys():
-                Contact.all_names23[n[1] + ' ' + n[2]].append([name, phone, email])
+                Contact.all_names23[n[1] + ' ' + n[2]].append(Contact.id)
             else:
-                Contact.all_names23[n[1] + ' ' + n[2]] = [[name, phone, email]]
+                Contact.all_names23[n[1] + ' ' + n[2]] = [Contact.id]
         if phone != '':
             if phone in Contact.all_phones.keys():
-                Contact.all_phones[phone].append([name, phone, email])
+                Contact.all_phones[phone].append(Contact.id)
             else:
-                Contact.all_phones[phone] = [[name, phone, email]]
+                Contact.all_phones[phone] = [Contact.id]
         else:
-            Contact.no_phones.append([name, phone, email])
+            Contact.no_phones.append(Contact.id)
         if email != '':
             if email in Contact.all_emails.keys():
-                Contact.all_emails[email].append([name, phone, email])
+                Contact.all_emails[email].append(Contact.id)
             else:
-                Contact.all_emails[email] = [[name, phone, email]]
+                Contact.all_emails[email] = [Contact.id]
         else:
-            Contact.no_emails.append([name, phone, email])
+            Contact.no_emails.append(Contact.id)
 
     def change(self, id, new_account):
         old_name = Contact.everybody[id][0]
@@ -57,7 +59,22 @@ class Contact:
         email = Contact.everybody[id][2]
         new_phone = new_account[1]
         new_email = new_account[2]
-        old_name = old_name.split()
+        old_name_list = old_name.split()
+        Contact.everybody[id][0] = new_account
+        if old_phone!=new_phone:
+            if old_phone=='':
+                Contact.no_phones.remove(id)
+                Contact.all_phones[new_phone].append(id)
+            else:
+                Contact.all_phones[old_phone].remove(id)
+                Contact.all_phones[new_phone].append(id)
+        if email!=new_email:
+            if email=='':
+                Contact.no_emails.remove(id)
+                Contact.all_emails[new_email].append(id)
+            else:
+                Contact.all_emails[email].remove(id)
+                Contact.all_emails[new_email].append(id)
 
 
 
@@ -73,8 +90,6 @@ for x in f:
     pd = x.split(',')
     user = Contact(pd[0], pd[1][1::], pd[2])
 f.close()
-
-print(Contact.change(user, 3, 90909), '00')
 
 print('Чтобы выполнить команду выберите 1- найти по номеру телефона, 2- найти по email, 3- найти по имени,'
       '4- найти по фамилии, 5- найти по Фамилия Имя, 6- найти по Имя Отчество, 7- редактирование профиля,'
@@ -96,6 +111,14 @@ elif task == 4:
     print('Введите фамилию пользователя')
     task_name = input()
     print('По вашему запросу нашлось: ', *Contact.all_names1[task_name])
+elif task == 5:
+    print('Введите фамилию и имя пользователя')
+    task_name = input()
+    print('По вашему запросу нашлось: ', *Contact.all_names12[task_name])
+elif task == 6:
+    print('Введите имя и отчество пользователя')
+    task_name = input()
+    print('По вашему запросу нашлось: ', *Contact.all_names23[task_name])
 elif task == 8:
     print(Contact.no_phones)
 elif task == 9:
@@ -104,6 +127,5 @@ if task == 7:
     print(Contact.everybody)
     task_id = input('Введите id изменяемого профиля')
     task_after = list(input('Введите желаемые значения для данного профиля').split())
-    task_before = Contact.everybody[task_id]
-    task2 = ['Иванов Иван Иванович', ' +79999999995', ' ivanov@ma.il']
+
 print(Contact.everybody)
